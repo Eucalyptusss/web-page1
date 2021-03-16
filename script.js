38775248855552c2cdb72cf3fd4c350dd3e94fa0
Glitch (hello-webpage) <none>
2021-03-16 00:33:14 +0000

    🌲🎗 Checkpoint ./style.css:29780824/430 ./script.js:29780824/271

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
+//Global Variables
+var pattern=[2, 2, 4, 3, 2, 1, 2, 4];
+var progress =0;
+var gamePlaying = false;
 
-// prints "hi" in the browser's dev tools console
-console.log("hi");
+
