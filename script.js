18c60c868157dceda3fd68fe83f66ed3b63c1a6b
Glitch (hello-webpage) <none>
2021-03-18 00:47:35 +0000

    🎾🏄 Checkpoint ./script.js:29780824/850 ./index.html:29780824/43

--- script.js
+++ script.js
@@ -2,5 +2,22 @@
 var pattern=[2, 2, 4, 3, 2, 1, 2, 4];
 var progress =0;
 var gamePlaying = false;
+var tonePlaying=false;
+var volume=.5;
+
+function startGame(){
+  // initialize game variables
+  progress=0;
+  gamePlaying = true;
+  document.getElementById("startBtn").classList.add("hidden");
+document.getElementById("stopBtn").classList.remove("hidden");
+}
+function stopGame(){  
+  gamePlaying = false;
+  document.getElementById("startBtn").classList.remove("hidden");
+document.getElementById("stopBtn").classList.add("hidden");
+  
+}
+// swap the Start and Stop BUttons
 
 
